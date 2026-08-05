88d08dc5efe00068d0ec2bba661c9913feab7a3e
Leo Wong <pakyinwww@gmail.com>
2026-08-05 12:45:45 +0100

    fix: update contact and text

--- src/i18n/locales/zhHant.js
+++ src/i18n/locales/zhHant.js
@@ -66,7 +66,7 @@ export default {
   contact: {
     title: '聯絡我們',
     email: '電郵',
-    emailDesc: '歡迎隨時電郵至 software{\'@\'}hanamaru.hk 聯絡我們。',
+    emailDesc: '歡迎隨時電郵至 solutions{\'@\'}hanamaru.hk 聯絡我們。',
     sendEmail: '發送電郵',
     whatsapp: 'WhatsApp',
     chatOnWhatsApp: 'WhatsApp 對話',
